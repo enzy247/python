491e5164ca9de436f317e8437ebee8a066840856
enzy247 <147549768+enzy247@users.noreply.github.com>
2024-04-08 12:03:33 +0300

    Add files via upload

---
+++ 11.py
@@ -0,0 +1,8 @@
+arr = []
+
+for i in range(10):
+    element = int(input(f"Введите элемент {i+1}: "))
+    arr.append(element)
+
+print("Массив, заполненный с клавиатуры:", arr)
+
---
+++ 12.py
@@ -0,0 +1,11 @@
+
+import random
+
+arr = []
+
+for i in range(15):
+    element = random.randint(1, 100)  
+    arr.append(element)
+
+print(arr)
+
